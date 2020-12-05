4fe1a2e4828009a02a2f27f2628d4ee241e96a1f
skck0226 <skck0226@ajou.ac.kr>
2020-12-05 11:48:46 +0000

    SyncStateByEffect&PrintWinner&Can'tClickAlreadySelected&NowCanResetGame

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 function Square(props){
 	return (
 		<button className="square" onClick={props.onClick} >	
@@ -102,6 +103,7 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
